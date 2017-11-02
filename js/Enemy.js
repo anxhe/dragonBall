5055287cb79e66016225bd15a35219fc5356c690
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-11-03 00:58:53 +0100

    Add uncomitted changes to Enemy

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,7 +1,14 @@
 function Enemy(position) {
-  this.width = 100;
-  this.height = 100;
-  this.maxSize = 700 - this.width;
+  this.ctx = context;
+  this.img = new Image();
+  this.img.src = "images/freezer.png";
+  this.width = 30;
+  this.height = 30;
   this.position = position;
   this.strong = 10;
+}
+
+
+Enemy.prototype.draw = function(){
+  this.ctx.drawImage(this.img, this.position.x, this.position.y, this.width, this.height);
 }
