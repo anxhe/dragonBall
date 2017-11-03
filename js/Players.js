2f12f76f58cc5f6924a3eeab4afa66eea1361377
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-11-03 09:02:35 +0100

    Add direction for players

--- js/Players.js
+++ js/Players.js
@@ -1,19 +1,25 @@
 function Players(name, position, keys){
   this.name = name;
   this.position = position;
-  this.width = 100;
-  this.height = 100;
-  this.speed = 100;
+  this.imagePosition = { x: null, y: null };
+  this.width = 50;
+  this.height = 50;
+  this.speed = 50;
   this.keys = keys ;
-  this.img = new Image();
+  this.img = new Image(32, 48);
   this.img.src = "images/" + name + ".png";
   this.score = 0;
   this.life = 100;
 }
 
 Players.prototype.draw = function(){
-  context.drawImage(this.img, this.position.x, this.position.y, 100, 100);
-
+  context.drawImage(
+    this.img,                                   // Imagen
+    this.imagePosition.x, this.imagePosition.y, // Coordenadas iniciales del sprite
+    this.img.width, this.img.height,            // Medidas de la imagen en el sprite
+    this.position.x + 8, this.position.y,       // Coordenadas iniciales en el canvas
+    this.img.width, this.img.height             // Medidas de la imagen en el canvas
+   );
 }
 
 Players.prototype.checkBoundaries = function(e){
@@ -33,15 +39,23 @@ Players.prototype.move = function(e) {
   switch (this.checkBoundaries(e) && e.keyCode) {
     case this.keys.LEFT:
       this.position.x -= this.speed;
+      this.imagePosition.x = 0;
+      this.imagePosition.y = 48;
       break;
     case this.keys.RIGHT:
       this.position.x += this.speed;
+      this.imagePosition.x = 0;
+      this.imagePosition.y = 96;
       break;
     case this.keys.UP:
       this.position.y -= this.speed;
+      this.imagePosition.x = 0;
+      this.imagePosition.y = 144;
       break;
     case this.keys.DOWN:
       this.position.y += this.speed;
+      this.imagePosition.x = 0;
+      this.imagePosition.y = 0;
       break;
   }
 }
